53091e5ad3914a54d5249ebb775a7f577afda3f9
Ishika <reshu234singh@gmail.com>
2026-02-22 03:51:37 -0500

    added Contract pages

--- ASPDOTNETProjrct/Controllers/HomeController.cs
+++ ASPDOTNETProjrct/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace ASPDOTNETProjrct.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
